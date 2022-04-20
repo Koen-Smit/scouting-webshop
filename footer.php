a5d4038d8fb29346c202eb1d6d71dc3ab7161f10
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-04-20 13:15:27 +0200

    header en footer bijgewerkt

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="footer-group">
+        <a href="">lorem1</a>
+        <a href="">lorem2</a>
+        <a href="">lorem3</a>
+    </div>
+    <div class="footer-group">
+        <a href="">lorem1</a>
+        <a href="">lorem2</a>
+        <a href="">lorem3</a>
+    </div>
+    <div class="footer-group">
+        <a href="">lorem1</a>
+        <a href="">lorem2</a>
+        <a href="">lorem3</a>
+    </div>
+    <div class="footer-group">
+        <a href="">lorem1</a>
+        <a href="">lorem2</a>
+        <a href="">lorem3</a>
+    </div>
+</footer>
--- header.php
+++ header.php
@@ -1,8 +1,13 @@
 <header>
+    <div class="logo-header">
+        <img src="img/scouting.jpg">
+    </div>
+    <div class="navigatie-top">    
     <nav>
         <a href="index.php">Home</a>
         <a href="">Webshop</a>
         <a href="overzicht.php">Overzicht</a>
         <a href="">fdsfsfs</a>
     </nav>
+    </div>
 </header>
