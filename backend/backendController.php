d8246385f81dc43e5b3a2f57c86634cdea62893d
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-05-03 21:07:48 +0200

    Backend aanpassingen

--- backend/backendController.php
+++ backend/backendController.php
@@ -1,7 +1,33 @@
 <?php
-require_once 'conn.php';
-$query ="SELECT * FROM orders ";
-$statement = $conn->prepare($query);
-$statement->execute();
-$users = $statement->fetchall(PDO::FETCH_ASSOC);
+session_start();
+if(!isset($_SESSION['user_id']))
+{
+    $msg = "Je moet eerst inloggen";
+    header("Location: ../inlogpagina.php");
+    exit;
+}
+
+$action= $_POST['action'];
+
+if ($action == "add")
+{   
+    $product_id= $_POST['product-id'];
+    $amount = $_POST['amount'];
+    
+    
+    if($amount == null || $amount == 0)
+    {
+        die("geen geldig getal gekozen");
+    }
+
+    $_SESSION['cart']=
+    [
+        $product_id => $amount
+    ];
+    echo $amount;
+    // header("Location: ../index.php");
+
+
+}
+
 ?>
