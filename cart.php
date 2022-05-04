55ddac766117e150374c67a6ac61a0bc14182971
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-05-04 18:18:11 +0200

    stylin cart

--- cart.php
+++ cart.php
@@ -1,8 +1,19 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Document</title>
+</head>
+<?php require_once "header.php"; ?>
 
-
+<div class="cart">
 <table>
     <thead>
         <tr>
@@ -31,6 +42,7 @@ session_start();
             $totalPrice = $totalPrice + $totalProductPrice;
 
             ?>
+            
             <tr>
                 <td><?php echo $product['name']; ?></td>
                 <td><?php echo $product['description']; ?></td>
@@ -41,25 +53,28 @@ session_start();
             <?php
         }
     ?>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>&euro;<?php echo $totalPrice; ?>,-</td>
-    </tr>
-    </tbody>
-</table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>&euro;<?php echo $totalPrice; ?>,-</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<div class="verzend-cart">
 <form action="backend/backendcontroller.php" method="POST">
     <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email" style="width: 200px;">
         <input type="hidden" name="action" value="create">
         <input type="hidden" name="status" value="0">
         <input type="hidden" name="cart" value="<?php $_SESSION['cart']?>">
     </div>
     <input type="submit" value="Bestel">
 </form>
+</div>
 
 
 
